1e76102c76d4579a522c7f2a6f2aff6bc5ca7648
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2021-02-20 17:05:02 +0300

    creating web pages template

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// selecting dom elements
+
+const toggleButton = document.getElementById('togglt-menu');
+const navLinks = document.getElementById('nav-bar')
+const toggleSubMenu = document.getElementById('show-submenu')
+const subMenu = document.getElementById('sub-menu-links')
+
+// function to show and hide navbar
+const showMobileNavBar =()=> navLinks.classList.toggle('nav-bar_toggle');
+toggleButton.addEventListener('click', showMobileNavBar);
+// function to show and hide submenu
+const showSubmenu =()=> {subMenu.classList.toggle('sub-menu-toggle')};
+toggleSubMenu.addEventListener('click', showSubmenu, showMobileNavBar);
