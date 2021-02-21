ed3637a1b0ba21d08363a3843a3549353515559d
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2021-02-21 17:48:28 +0300

    updates to index page mobile view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,7 @@ const toggleButton = document.getElementById('togglt-menu');
 const navLinks = document.getElementById('nav-bar')
 const toggleSubMenu = document.getElementById('show-submenu')
 const subMenu = document.getElementById('sub-menu-links')
+const thisYear = document.getElementById('year');
 
 // function to show and hide navbar
 const showMobileNavBar =()=> navLinks.classList.toggle('nav-bar_toggle');
@@ -11,3 +12,8 @@ toggleButton.addEventListener('click', showMobileNavBar);
 // function to show and hide submenu
 const showSubmenu =()=> {subMenu.classList.toggle('sub-menu-toggle')};
 toggleSubMenu.addEventListener('click', showSubmenu, showMobileNavBar);
+
+// Automatic updated date
+
+const date = new Date();
+thisYear.textContent = date.getFullYear()
